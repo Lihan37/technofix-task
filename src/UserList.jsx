5f539e38e449f288a4613f0760d9cae0f2108b25
eanur rahman <lihan.lihu@gmail.com>
2024-02-10 14:49:58 +0600

    fixed some codes

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -14,9 +14,9 @@ const UserList = () => {
     email: "",
     address: "",
     companyName: "",
-    image: null
+    image: null,
   });
-  const [selectedUser, setSelectedUser] = useState(null); // State for selected user
+  const [showForm, setShowForm] = useState(false); 
 
   useEffect(() => {
     fetchUsers();
@@ -41,7 +41,7 @@ const UserList = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const newUser = {
       id: Math.floor(Math.random() * 1000) + 1,
       firstName: formData.firstName,
@@ -49,21 +49,22 @@ const UserList = () => {
       email: formData.email,
       address: { address: formData.address },
       company: { name: formData.companyName },
-      image: formData.image ? URL.createObjectURL(formData.image) : null
+      image: formData.image ? URL.createObjectURL(formData.image) : null,
     };
-  
+
     setUsers([...users, newUser]);
-    setSelectedUser(newUser); 
+
     Swal.fire("Success", "User added successfully", "success");
-  
+
     setFormData({
       firstName: "",
       lastName: "",
       email: "",
       address: "",
       companyName: "",
-      image: null
+      image: null,
     });
+    setShowForm(false); 
   };
 
   const handleChange = (e) => {
@@ -95,137 +96,147 @@ const UserList = () => {
     <div>
       <Navbar />
       <div className="m-5">
-        <input
-          type="text"
-          placeholder="Search by name"
-          className="border ml-4 border-gray-300 rounded-md py-2 px-4 mb-4 focus:outline-none focus:border-blue-500 bg-white shadow-md"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-        <select
-          value={sortBy}
-          onChange={handleSortBy}
-          className="border ml-4 border-gray-300 rounded-md py-2 px-4 mb-4 focus:outline-none focus:border-blue-500 bg-white shadow-md"
-          style={{ minWidth: "200px" }}
-        >
-          <option value="name">Sort by name</option>
-          <option value="email">Sort by email</option>
-          <option value="company">Sort by Company name</option>
-        </select>
-      </div>
-      <form
-        className="bg-white shadow-md rounded md:w-2/4 lg:w-1/4 mx-auto px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleSubmit}
-      >
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="firstName"
-          >
-            First Name
-          </label>
+        <div className="flex flex-wrap items-center justify-between mb-4">
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="firstName"
             type="text"
-            placeholder="First Name"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
+            placeholder="Search by name"
+            className="border mb-2 md:mb-0 md:mr-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500 bg-white shadow-md"
+            value={searchTerm}
+            onChange={handleSearch}
           />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="lastName"
+          <select
+            value={sortBy}
+            onChange={handleSortBy}
+            className="border mb-2 md:mb-0 md:mr-2 border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500 bg-white shadow-md"
+            style={{ minWidth: "200px" }}
           >
-            Last Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="lastName"
-            type="text"
-            placeholder="Last Name"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="address"
-          >
-            Address
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="address"
-            type="text"
-            placeholder="Address"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="companyName"
-          >
-            Company Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="companyName"
-            type="text"
-            placeholder="Company Name"
-            name="companyName"
-            value={formData.companyName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="image"
-          >
-            Image
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="image"
-            type="file"
-            name="image"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex items-center justify-between">
+            <option value="name">Sort by name</option>
+            <option value="email">Sort by email</option>
+            <option value="company">Sort by Company name</option>
+          </select>
           <button
+            onClick={() => setShowForm(!showForm)}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
           >
-            Add User
+            {showForm ? "Close Form" : "Add User"}
           </button>
         </div>
-      </form>
+        {showForm && (
+          <form
+            className="bg-white shadow-md rounded md:w-2/4 lg:w-1/4 mx-auto px-8 pt-6 pb-8 mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="firstName"
+              >
+                First Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="firstName"
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="lastName"
+              >
+                Last Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="lastName"
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="address"
+              >
+                Address
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="address"
+                type="text"
+                placeholder="Address"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="companyName"
+              >
+                Company Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="companyName"
+                type="text"
+                placeholder="Company Name"
+                name="companyName"
+                value={formData.companyName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="image"
+              >
+                Image
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="image"
+                type="file"
+                name="image"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Add User
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5 mx-5">
         {filteredUsers.map((user) => (
           <div
@@ -239,8 +250,13 @@ const UserList = () => {
                 className="w-24 h-24 rounded-full mx-auto mb-4"
               />
               <p className="font-bold text-lg text-blue-500">
-                <a href="#" onClick={() => setSelectedUser(user)}>{user.firstName} {user.lastName}</a>
+                {user.firstName} {user.lastName}
               </p>
+              <Link to={`/user/${user.id}`}>
+                <p className="text-gray-600 font-bold text-lg">
+                  {user.username}
+                </p>
+              </Link>
               <p className="text-gray-600 text-sm">{user.email}</p>
               <div className="mt-2 text-sm">
                 <p className="mb-1">
@@ -257,42 +273,6 @@ const UserList = () => {
           </div>
         ))}
       </div>
-      {/* Modal for displaying user details */}
-      {selectedUser && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 transition-opacity">
-              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-            </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                    {/* User Image */}
-                    <img className="h-10 w-10 rounded-full" src={selectedUser.image} alt="" />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">{selectedUser.firstName} {selectedUser.lastName}</h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">{selectedUser.email}</p>
-                    </div>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500"><strong>Address:</strong> {selectedUser.address.address}, {selectedUser.address.suite}, {selectedUser.address.city}</p>
-                      <p className="text-sm text-gray-500"><strong>Company:</strong> {selectedUser.company.name}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="button" onClick={() => setSelectedUser(null)} className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
